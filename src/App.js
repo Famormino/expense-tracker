191534cba4a6995ad5c7011c3ec055716967c8a9
Federico Amormino <federico.amormino@gmail.com>
2022-06-30 12:48:50 -0300

    adding NewExpense Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -23,6 +24,7 @@ const App = () => {
   ]
   return (
     <div className="App">
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
