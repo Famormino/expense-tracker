eb5ccef1277f44f4719d48a3a93db2d0d2b8abb1
Federico Amormino <federico.amormino@gmail.com>
2022-06-30 09:25:01 -0300

    adding state in ExpenseItem

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,15 +1,21 @@
+import { useState } from 'react'
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItems.css'
 
 const ExpenseItems = (props) => {
+    const [nombre, setNombre] = useState(props.nombre)
 
+    const handleChange = () => {
+        setNombre('Updated')
+    }
     return (
         <Card className='expense-item'>
             <ExpenseDate fecha={props.fecha} />
             <div className='expense-item__description'>
-                <h2>{props.nombre}</h2>
-                <div className='expense-item__price'>${props.costo} </div>
+                <h2>{nombre}</h2>
+                <button onClick={handleChange}>Click me!</button>
+                <div className='expense-item__price'>${props.costo}</div>
             </div>
         </Card>
     )
